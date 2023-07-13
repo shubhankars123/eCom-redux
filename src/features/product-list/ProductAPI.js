090092de3f84d2d292f2feec646346c00126607d
Shubhankar <satpathyshubhankar@gmail.com>
2023-07-13 21:12:12 +0530

    first commit

--- src/features/product-list/ProductAPI.js
+++ src/features/product-list/ProductAPI.js
@@ -1,10 +1,45 @@
-const fetchAllProducts =() => {
-    return new Promise( async(resolve) =>{
+export const fetchAllProducts = () => {
+    return new Promise(async (resolve) => {
         // TODO : we will not hard-code server here
         const response = await fetch("http://localhost:8080/products");
         const data = await response.json()
-        resolve({data})
-        } 
+        resolve({ data })
+    }
     );
 }
-export default fetchAllProducts
+
+
+export const fetchProductsByFilters = (filter, sort, pagination) => {
+    // filter = {"category" : "smartphones"}
+    // sort = {_sort : "price", _order="desc"}
+    // pagination = {_page  :1, _limit=10}
+
+    let queryString = '';
+    for (let key in filter) {
+        const categoryValues = filter[key]
+        if(categoryValues.length){
+            const lastCategoryValue = categoryValues[categoryValues.length-1]
+            queryString += `${key}=${lastCategoryValue}&`
+        }
+    }
+    
+    for(let key in sort){
+        queryString += `${key}=${sort[key]}&`
+
+    }
+
+    for(let key in pagination){
+        queryString += `${key}=${sort[key]}&`
+
+    }
+
+    return new Promise(async (resolve) => {
+        // TODO : we will not hard-code server here
+        const response = await fetch("http://localhost:8080/products?"+queryString);
+        const data = await response.json()
+        const totalItems = await response.headers.get('X-Total-Count')
+        resolve({ data : {products : data, totalItems: + totalItems} })
+    }
+    );
+}
+
--- src/features/product-list/productSlice.js
+++ src/features/product-list/productSlice.js
@@ -1,19 +1,32 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import fetchAllProducts from "./ProductAPI.js"
+import { fetchAllProducts, fetchProductsByFilters } from "./ProductAPI.js"
 
 const initialState = {
     products: [],
     status: "idle",
+    totalItems : 0
 };
 
+// The value we return becomes the 'fulfilled action payload
 export const fetchAllProductsAsync = createAsyncThunk(
     'product/fetchAllProducts',
     async () => {
         const response = await fetchAllProducts();
-        // The value we return becomes the 'fulfilled action payload
+        return response.data;  
+    }
+)
+
+// The value we return becomes the 'fulfilled action payload
+export const fetchProductsByFilterAsync = createAsyncThunk(
+    'product/fetchAllProductsByFilters',
+    async ({filter, sort, pagination}) => {
+        const response = await fetchProductsByFilters(filter, sort, pagination);
+        // The value we return becomes the `fulfilled` action payload
         return response.data;
     }
 )
+
+
 
 export const productSlice = createSlice({
     name: "product",
@@ -25,21 +38,25 @@ export const productSlice = createSlice({
     },
 
     extraReducers: (builder) => {
-        builder
-        .addCase(fetchAllProductsAsync.pending, (state) => {
-                 state.status = 'loading';
-            })
-            .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-               
-               state.status = 'idle'
-               state.products = action.payload
-              
-            })
-        }
+        builder.addCase(fetchAllProductsAsync.pending, (state) => {
+            state.status = 'loading';
+        }).addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
+            state.status = 'idle'
+            state.products = action.payload
+        }).addCase(fetchProductsByFilterAsync.pending, (state) => {
+            state.status = 'loading';
+        }).addCase(fetchProductsByFilterAsync.fulfilled, (state, action) => {
+            state.status = 'idle'
+            state.products = action.payload.products
+            state.totalItems = action.payload.totalItems
+        })
+
+    }
 })
 
 export const { increment } = productSlice.actions;
 
 export const selectAllProducts = (state) => state.product.products;
+export const selectTotalItems = (state) => state.product.totalItems;
 
 export default productSlice.reducer
